4122111cc25338c4eefd84af9c8ad4e69934fc78
Shiue-Yuan Chuang <josephj1o4e1.goofygroove@gmail.com>
2024-02-27 06:21:58 +0000

    dbt tests

---
+++ tmp.sql
@@ -0,0 +1,8 @@
+{% set models_to_generate = codegen.get_models(directory='staging', prefix='stg') %}
+{{
+    codegen.generate_model_yaml(
+        model_names = models_to_generate
+    )
+}}
+
+-- compile the above
